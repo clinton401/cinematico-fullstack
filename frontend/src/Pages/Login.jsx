dfd65cdf509976fa1a5d60aa51b78fd2687a71a8
clinton <clintonphillips464@gmail.com>
2024-08-22 05:01:30 +0100

    fixing the redirect code

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -64,7 +64,8 @@ function Login() {
           email: "",
           password: "",
         });
-        window.location.href = data.redirectUrl;
+        // window.location.href = data.redirectUrl;
+        window.location.reload()
       
         
        
