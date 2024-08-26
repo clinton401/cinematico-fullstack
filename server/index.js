072c6d9e98da61a7fb823ebadf65c322d51f7205
clinton <clintonphillips464@gmail.com>
2024-08-26 20:39:02 +0100

    moved the auth routes declaration to its initial position

--- server/index.js
+++ server/index.js
@@ -23,14 +23,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 // API routes
-
+app.use("/auth", authRouter);
 // app.use("/api", apiRouter);
 
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
-app.use("/auth", authRouter);
 
 
 app.all("*", notFound);
